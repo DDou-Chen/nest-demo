028196cec4bc4dbecee01f2f502be500fb8565cb
兜兜 <913321406@qq.com>
2024-07-21 16:53:23 +0800

    feat: rxjs和interceptor--class.19

---
+++ src/aaa.interceptor.ts
@@ -0,0 +1,26 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Inject,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, tap } from 'rxjs';
+import { AppService } from './app.service';
+
+@Injectable()
+export class AaaInterceptor implements NestInterceptor {
+  // constructor(private appService: AppService) {}
+
+  @Inject()
+  private appService: AppService;
+
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log(this.appService.getHello());
+
+    const now = Date.now();
+    return next
+      .handle()
+      .pipe(tap(() => console.log(`After...${Date.now() - now}ms`)));
+  }
+}
