62d3c975b1bf991bf4744b5d3c5b9b3af5617af0
兜兜 <913321406@qq.com>
2025-04-17 11:15:55 +0800

    sse推送消息到客户端
    react-pass 的 seePage 配合测试

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Sse } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Observable } from 'rxjs';
+import { exec } from 'child_process';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,31 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Sse('stream')
+  stream() {
+    return new Observable((observer) => {
+      observer.next({ data: { msg: 'aaa' } });
+
+      setTimeout(() => {
+        observer.next({ data: { msg: 'bbb' } });
+      }, 2000);
+
+      setTimeout(() => {
+        observer.next({ data: { msg: 'ccc' } });
+      }, 4000);
+    });
+  }
+
+  @Sse('stream2')
+  stream2() {
+    const childProcess = exec('tail -f ./log2');
+
+    return new Observable((observer) => {
+      childProcess.stdout.on('data', (msg) => {
+        observer.next({ data: { msg: msg.toString() } });
+      });
+    });
   }
 }
