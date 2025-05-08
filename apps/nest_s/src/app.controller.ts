e1ae3994085b65820a2b4038406e606429009ccb
兜兜 <913321406@qq.com>
2025-05-08 14:29:11 +0800

    微服务monorepo和library

--- apps/nest_s/src/app.controller.ts
+++ apps/nest_s/src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Lib1Service } from '@app/lib1';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Inject()
+  private lib1Service: Lib1Service;
+
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('aaa')
+  aaa() {
+    return 'aaa' + this.lib1Service.xxx();
   }
 }
---
+++ libs/lib1/src/index.ts
@@ -0,0 +1,3 @@
+// 导出 module、service
+export * from './lib1.module';
+export * from './lib1.service';
